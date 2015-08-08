35cb446dc15cdd3402ec52438b0cd9613d302dd0
gdsanders <gd@digitalequity.com>
2015-08-08 11:32:51 -0500

    Outlets connected

--- ViewController.swift
+++ ViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var makeItBlueButton: UIButton!
+    @IBOutlet weak var fireLasersButton: UIButton!
+    @IBOutlet weak var imageOnlyButton: UIButton!
+    @IBOutlet weak var alternativeImageButton: UIButton!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +29,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func makeItBlueButtonPressed(sender: UIButton) {
+        view.backgroundColor = UIColor.blueColor()
+        
+        makeItBlueButton.setTitle("It is now BLUE!", forState: UIControlState.Normal)
+    }
+    
+    @IBAction func fireTheLasersButtonPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func imageOnlyButtonPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func alternativeImageButtonPressed(sender: UIButton) {
+        
+    }
+    
 
 
 }
